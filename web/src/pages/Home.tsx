ca781b471d6e137fdf320f883aedf67c51e9abdf
Sharath <sharath13feb@gmail.com>
2025-03-15 13:25:59 +0530

    profile card

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import {Feed} from '../components/feeds/Feed'
 import { ProfileCard } from '@/components/ProfileCard'
+import {Feed} from '../components/feeds/Feed'
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-red-300">
-      {/* <ProfileCard/> */}
+      <ProfileCard />
       <div className="flex"> 
         <div className="lg:ml-[21rem]"> 
           <Feed />
