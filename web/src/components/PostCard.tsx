b08679547857d6f7d577b45a53602b08d483b3a9
Ayush <ayushranaut008@gmail.com>
2025-03-23 17:20:09 +0530

    fixed postcard redirecting

--- web/src/components/PostCard.tsx
+++ web/src/components/PostCard.tsx
@@ -115,11 +115,8 @@ export default function PostCard({ post, isAdmin, onPostDelete}: PostCardProps) 
   return (
     <div
       className="max-w-[700px] mx-auto rounded-xl bg-white dark:bg-neutral-800 dark:border-neutral-900 dark:shadow-0 dark:shadow-sm p-4 shadow-lg mb-4 border border-gray-200"
-      onClick={(e) => {
-        e.stopPropagation();
-        const basePath = isAdmin ? '/admin/profile/' : '/profile/';
-        navigate(`${basePath}${post.postedBy}`);
-      }}
+
+      
     >
       <div className="flex items-center justify-between mb-4">
         <div className="flex items-center">
