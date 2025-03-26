ef81f96a260e38ca31e3654f900b32847a17d0f5
Ayush <ayushranaut008@gmail.com>
2025-03-27 00:24:48 +0530

    fixed styling in home page

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -8,7 +8,7 @@ export const Home = () => {
     return null;
   
   return (
-    <div className="relative px-8">
+    <div className="relative px-8 bg-white dark:bg-neutral-950 dark:border-neutral-900">
       {/* ProfileCard shifted to the right */}
       <div className="absolute left-40 -top-12 ">
         <ProfileCard userInfo={authUser} isOwnProfile={true} />
