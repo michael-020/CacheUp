8d7500d3ca747bfc5ffd86b6dcac1148a3e08b3a
Michael Hosamani <michaelhosamani26@gmail.com>
2025-04-10 12:31:27 +0530

    added responsivenss to message page

--- web/src/pages/Messages.tsx
+++ web/src/pages/Messages.tsx
@@ -9,7 +9,7 @@ export const Messages = () => {
 
   return (
     <div className="flex items-center justify-center pt-24 px-4 pb-6 h-screen dark:bg-neutral-950">
-        <div className="rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+        <div className="rounded-lg pb-24 lg:pb-0 shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden border shadow-xl dark:border-neutral-800 dark:shadow-neutral-700 dark:shadow-md">
             <ChatSidebar />
 
