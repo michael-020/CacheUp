6d0547bc2135932d5f123764f29d12b609ece80e
Michael Hosamani <michaelhosamani26@gmail.com>
2025-04-25 23:17:27 +0530

    added responsive ness to admin navbar

--- web/src/pages/admin/Statistics.tsx
+++ web/src/pages/admin/Statistics.tsx
@@ -26,7 +26,7 @@ const Statistics = () => {
           <h1 className="text-2xl font-bold">User Statistics</h1>
         </div>
         
-        <div className="mt-24 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-24">
           <UserStats />
         </div>
       </div>
