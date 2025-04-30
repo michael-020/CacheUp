afe31310c60abbd95897434658d43e188404efa2
Michael Hosamani <michaelhosamani26@gmail.com>
2025-04-30 13:44:05 +0530

    added friend suggestion slider

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -6,14 +6,13 @@ import { routeVariants } from '@/lib/routeAnimation';
 import FriendSuggestions from '@/components/FriendsSuggestions';
 
 export const Home = () => {
-  const { authUser } = useAuthStore()
+  const { authUser } = useAuthStore();
 
-  if(!authUser)
-    return null;
+  if (!authUser) return null;
 
   return (
     <motion.div  
-      className="relative px-8 bg-gray-100 dark:bg-neutral-950 dark:border-neutral-900"
+      className="relative px-4 sm:px-8 bg-gray-100 dark:bg-neutral-950 dark:border-neutral-900"
       variants={routeVariants}
       initial="initial"
       animate="final"
@@ -23,14 +22,13 @@ export const Home = () => {
         <ProfileCard userInfo={authUser} isOwnProfile={true} />
       </div>
 
-      <div className="ml-[1rem] pb-36 md:pb-36 lg:pb-20">
+      <div className="ml-0 lg:ml-[1rem] pb-36 md:pb-36 lg:pb-20">
         <Feed />
       </div>
 
       <div className="absolute right-2 top-24 hidden lg:block">
         <FriendSuggestions />
       </div>
-
     </motion.div>
   );
 };
