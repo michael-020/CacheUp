29b46c82c40303d5aae65286e88e8d105617bbf1
Michael Hosamani <michaelhosamani26@gmail.com>
2025-05-06 11:24:54 +0530

    added entry-client in web for ssr

---
+++ web/src/entry-client.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { useForumStore } from './stores/ForumStore/useforumStore';
+
+// Get initial state that was injected into the HTML
+const initialState = (window as any).__INITIAL_STATE__;
+
+// Initialize store with server state before hydration
+if (initialState) {
+  useForumStore.setState({
+    ...useForumStore.getState(),
+    ...initialState,
+    loading: false
+  });
+}
+
+hydrateRoot(
+  document.getElementById('root')!,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
