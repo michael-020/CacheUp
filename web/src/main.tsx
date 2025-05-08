9405e349f189a4c75141a21f9d0d7dc8a0321ce2
Michael Hosamani <michaelhosamani26@gmail.com>
2025-05-08 21:03:46 +0530

    updated main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // Make sure App.tsx exists in the same directory
 import './index.css';     // Optional: import global styles
@@ -7,7 +6,5 @@ const rootElement = document.getElementById('root') as HTMLElement;
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
